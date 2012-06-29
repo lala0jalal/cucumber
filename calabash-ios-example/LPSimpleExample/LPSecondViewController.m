47cec460734adedb92af7fbe8210190f674d0961
jenkins <jenkins@email.com>
2012-06-29 10:57:30 -0400

    commit

---
+++ calabash-ios-example/LPSimpleExample/LPSecondViewController.m
@@ -0,0 +1,82 @@
+//
+//  LPSecondViewController.m
+//  LPSimpleExample
+//
+//  Created by Karl Krukow on 07/10/11.
+//  Copyright (c) 2011 Trifork. All rights reserved.
+//
+#import <MapKit/MapKit.h>
+#import "LPSecondViewController.h"
+
+@implementation LPSecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"Second", @"Second");
+        self.tabBarItem.image = [UIImage imageNamed:@"second"];
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+- (void)mapView:(MKMapView *)aMapView didUpdateUserLocation:(MKUserLocation *)aUserLocation {
+    MKCoordinateRegion region;
+    MKCoordinateSpan span;
+    span.latitudeDelta = 0.005;
+    span.longitudeDelta = 0.005;
+    CLLocationCoordinate2D location;
+    location.latitude = aUserLocation.coordinate.latitude;
+    location.longitude = aUserLocation.coordinate.longitude;
+    region.span = span;
+    region.center = location;
+    [aMapView setRegion:region animated:YES];
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return YES;
+}
+
+@end
---
+++ calabash-ios-example/features/my_first.feature
@@ -0,0 +1,29 @@
+Feature: Running a test
+  As an iOS developer
+  I want to have a sample feature file
+  So I can begin testing quickly
+
+#Scenario: Example steps
+#  Given the app is running
+#    Then take picture
+
+
+Scenario: tab bar navigation
+  Given the app is running
+    Then I wait for 2.3 seconds
+    Then I touch "First" 
+    Then I wait for 2.3 seconds
+    Then I fill in "Name" with "text to write" 
+    Then I wait for 2.3 seconds
+    Then I touch "login"  
+    Then I wait for 2.3 seconds
+    Then take picture
+    Then I wait for 2.3 seconds
+    Then I touch "Third"
+    Then take picture
+    Then I swipe up on "Cell 0"    
+     Then I wait for 2.3 seconds
+   
+
+
+
